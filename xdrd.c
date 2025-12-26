cfc72cfcd1ebf2a301cc00d9850d265655c995a9
Fabrizio Di Pilla <fabriziodipilla@gmail.com>
2025-12-26 11:30:13 -0300

    fix: warning: ignoring return value of ‘system’

--- xdrd.c
+++ xdrd.c
@@ -806,7 +806,8 @@ user_t* user_add(server_t* LIST, int fd, int auth)
     if(server.f_exec && LIST->online_auth == 1)
     {
         server_log(LOG_INFO, "executing: %s", server.f_exec);
-        system(server.f_exec);
+	ssize_t unused = system(server.f_exec);
+	(void) unused;
     }
 
     pthread_mutex_unlock(&LIST->mutex);
